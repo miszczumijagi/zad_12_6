ab14a46ead25359d59227a0d704e8cc2dfdc4324
Kamil Stangret <kamilstangret@wp.pl>
2018-01-20 21:13:14 +0100

    Add html, css and js files with contents

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+var url = 'https://restcountries.eu/rest/v2/name/';
+var countriesList = $('#countries');
+
+$('#search').click(searchCountries);
+
+function searchCountries() {
+ 	var countryName = $('#country-name').val();
+	if(!countryName.length) countryName = 'Poland';
+	$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
+
+function showCountriesList(resp) {
+	countriesList.empty();
+	resp.forEach(function(item){
+
+	   	$('<h3>').text(item.name).appendTo(countriesList);
+	   	$('<li>').text('country native name: ' + item.nativeName).appendTo(countriesList);
+		$('<li>').text('capital: ' + item.capital).appendTo(countriesList);
+		$('<li>').text('region: ' + item.region).appendTo(countriesList);
+		$('<li>').text('currency: ' + item.currencies[0].name +' (' + item.currencies[0].symbol + ')').appendTo(countriesList);
+		$('<li>').text('language: ' + item.languages[0].name).appendTo(countriesList);
+		$('<li>').text('area: ' +  item.area +  ' km 2').appendTo(countriesList);
+		var borders = item.borders;
+		var caBorders = borders.join(', ');
+		if (!caBorders.length) {
+			$('<li>').text('border lands: brak').appendTo(countriesList);
+			} else {
+			$('<li>').text('border lands: ' + caBorders).appendTo(countriesList);
+		}		
+});
+
+}
